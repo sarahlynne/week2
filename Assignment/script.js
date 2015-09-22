da061a47a3955d7e25b26509f7708c0e6f87c61a
Sarah <belc0017@umn.edu>
2015-09-22 23:40:27 +0000

    I got it to show up and added extra stuff!

--- Assignment/script.js
+++ Assignment/script.js
@@ -1,13 +1,20 @@
 var map = L.map('map-container').setView([44.971724, -93.243239], 16);
 
-L.tileLayer(http://{s}.tile.osm.org/{z}/{x}/{y}.png, {
-}).addTo(map);
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map);
 
-$.getJSON(<https://dl.dropboxusercontent.com/u/8550761/wilson-library.geojson>, function(data){
+$.getJSON("https://dl.dropboxusercontent.com/u/8550761/wilson-library.geojson", function(data){
 
 	var geojson = new L.geoJson(data).addTo(map);
 });
 
-$(<.box.red>).click(function(e){<doing something>}) {
-	alert("This is a border");
-};
+$(".box.red").click(function(e){alert("This is a box")});
+
+$.getJSON('green-line-eastbound.geojson', function(data){
+
+	var geojson = new L.geoJson(data).addTo(map);
+});
+
+$.getJSON("http://opendata.minneapolismn.gov/datasets/cb8d4b1dbad0470380e5f46f1e75e962_0.geojson", function(data){
+
+	var geojson = new L.geoJson(data).addTo(map);
+});
